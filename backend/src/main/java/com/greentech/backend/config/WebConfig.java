6a0c3ebde4cb1363fbdefd478c08916095214059
UlisesXXXI <166647630+UlisesXXXI31@users.noreply.github.com>
2026-06-06 06:19:50 +0000

    removed annotation @Value from WebConfig

--- backend/src/main/java/com/greentech/backend/config/WebConfig.java
+++ backend/src/main/java/com/greentech/backend/config/WebConfig.java
@@ -3,13 +3,11 @@ package com.greentech.backend.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.beans.factory.annotation.Value;
+
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-@Value("${app.cors.allowed-origins}")
-    private String allowedOrigins;
 
  @Override
     public void addCorsMappings(CorsRegistry registry) {
